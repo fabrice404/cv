5a07c2a01174c373ecb672ffad79664bcf36890f
fabrice404 <fabrice.lamant@gmail.com>
2017-04-24 21:34:28 +0200

    Add some contact info

--- src/components/ContactItem.jsx
+++ src/components/ContactItem.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import FontAwesome from 'react-fontawesome'
 
 const ContactItem = props => (
     <div className="item">
-        <span className="title">{props.value}</span>
+        <span className="title">
+            <FontAwesome name={props.icon}/> {props.value}
+        </span>
     </div>
 );
 
